9d21d4b2f8fc1369ec3d5041a267646017d93085
mio's PC <9just.k.on@gmail.com>
2019-04-13 13:11:14 +0800

    merge

--- Readme.md
+++ Readme.md
@@ -6,3 +6,7 @@ https://chih-ling-hsu.github.io/2018/05/14/NYC#q1-which-regions-have-most-pickup
 + Convert SHP to LatLong Online
 https://mygeodata.cloud/converter/shp-to-latlong
 
++ WSL 
+https://stackoverflow.com/questions/43397162/show-matplotlib-plots-in-ubuntu-windows-subsystem-for-linux
++ Choose color
+https://public.tableau.com/profile/chris.gerrard#!/vizhome/TableauColors/ColorPaletteswithRGBValues
--- data.py
+++ data.py
@@ -1,11 +1,14 @@
 import pandas as pd
 import shapefile
+import matplotlib
+import matplotlib.pyplot as plt
+from matplotlib.patches import Polygon
+# plt.style.use('ggplot')
 
 # source = pd.read_csv('Data/yellow_tripdata_2018-12.csv', \
 source = pd.read_csv('Data/temp.csv', \
 	dtype={'VendorID':'category','RatecodeID':'category','store_and_fwd_flag':'category','PULocationID':'category','DOLocationID':'category','payment_type':'category'}, \
 	parse_dates=["tpep_pickup_datetime","tpep_dropoff_datetime"])
-
 print(source.info())
 print("------------------------")
 # list first few rows (datapoints)
@@ -39,3 +42,4 @@ print("------------------------")
 print(trainData.describe())
 print("------------------------")
 print(testData.describe())
+
